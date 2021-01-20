0103eace7b3f246d63b9063b0c1aa5f9b0ab0b37
KSSBro <shashwatdeadcliff@gmail.com>
2021-01-21 01:18:58 +0530

    add: src: rudimentary code; update: package.json: added coco-ssd

---
+++ src/main.js
@@ -0,0 +1,40 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+
+app.on("ready", function() {
+    renderMainWindow();
+});
+
+renderMainWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 850,
+        height: 550,
+        resizable: false,
+        title: "Sharingan",
+        webPreferences: {
+            nodeIntegration: true
+        },
+        show: false
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "/components/main.window/index.html"),
+        protocol: "file:",
+        slashes: true
+    }));
+
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+    });
+
+    mainWindow.setMenuBarVisibility(false);
+
+    mainWindow.on("closed", function() {
+        app.quit();
+    });
+}
